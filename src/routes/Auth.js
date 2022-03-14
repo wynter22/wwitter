1683631f98478cb354f3b92a422c8878afcd0511
wynter22 <dev.u0lee@gmail.com>
2022-03-14 19:01:58 +0900

    firebase 연동 및 로그인 구현

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,2 +1,46 @@
-import React from "react";
-export default ()=> <span>Auth</span>;
+import { async } from "@firebase/util";
+import { authService } from "../firebase";
+import React, { useState } from "react";
+export default () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const onChange = (event) => {
+        const { target: { name, value } } = event;
+        if (name === 'email') {
+            setEmail(value)
+        } else if (name === 'password') {
+            setPassword(value)
+        };
+    };
+    const onSubmit = async (event) => {
+        let data;
+        console.log(email, password);
+        event.preventDefault();
+        try{
+            if (newAccount) {
+                const auth = authService.getAuth();
+                await authService.createUserWithEmailAndPassword(auth, email, password);
+    
+            } else {
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        }  catch(error){
+            console.error(error);
+        }
+    };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input name="email" type="email" placeholder="email" required value={email} onChange={onChange} />
+                <input name="password" type="password" placeholder="password" required value={password} onChange={onChange} />
+                <input type="submit" value="Log In" />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    )
+};
