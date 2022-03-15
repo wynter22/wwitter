9d8d39f3a56f51206105e5c267c3314a48e82cc0
wynter22 <dev.u0lee@gmail.com>
2022-03-15 19:05:56 +0900

    react 버전 변경 및 env설정 수정

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Router from "components/Router";
 import { authService } from "../firebase";
 
 function App() {
-  console.log(authService.currentUser);
+
   const [isLoggedIn, setIsLoggedIn] = useState(authService.getAuth.currentUser);
   return <>
     <Router isLoggedIn={isLoggedIn}/>
