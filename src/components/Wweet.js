991dbcf87781cbde6dc55234bb61be4b08f5026c
wynter22 <dev.u0lee@gmail.com>
2022-04-08 15:20:12 +0900

    wweet 수정 기능 구현

--- src/components/Wweet.js
+++ src/components/Wweet.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { db, storeService } from 'utils/firebase';
 
 const Wweet = ({ wweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newWweet, setNewWweet] = useState(wweetObj.text);
+
   const onDeleteClick = async () => {
     const ok = window.confirm('Are you sure you want to delete this wweet?');
     if (ok) {
@@ -9,14 +12,46 @@ const Wweet = ({ wweetObj, isOwner }) => {
     }
   };
 
+  const toggleEditing = () => setEditing(prev => !prev);
+  const onSubmit = async event => {
+    event.preventDefault();
+
+    await storeService.updateDoc(storeService.doc(db, 'wweets', wweetObj.id), {
+      text: newWweet,
+    });
+    setEditing(false);
+  };
+  const onChange = event => {
+    const {
+      target: { value },
+    } = event;
+    setNewWweet(value);
+  };
+
   return (
-    <>
-      <div>
-        <h4>{wweetObj.text}</h4>
-        {isOwner && <button onClick={onDeleteClick}>Delete Wweet</button>}
-        {isOwner && <button>Edit Wweet</button>}
-      </div>
-    </>
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your nweet"
+              value={newWweet}
+              required
+              onChange={onChange}
+            />
+            <input type="submit" value="Update Nweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{wweetObj.text}</h4>
+          {isOwner && <button onClick={onDeleteClick}>Delete Wweet</button>}
+          {isOwner && <button onClick={toggleEditing}>Edit Wweet</button>}
+        </>
+      )}
+    </div>
   );
 };
 
